571e645cfcd7e26fe2936bee2cd470e9843c5fd1
golden-expirirensu <gundari_dan@bk.ru>
2022-03-02 23:13:59 +0700

    created test with only env.init method

---
+++ program/tests/donation.test.rs
@@ -0,0 +1,78 @@
+#![cfg(feature = "test-bpf")]
+use borsh::{BorshDeserialize, BorshSerialize};
+use solana_program::pubkey::Pubkey;
+use solana_program::system_instruction;
+use solana_program_test::{processor, tokio, ProgramTest, ProgramTestContext};
+use solana_sdk::signature::{Keypair, Signer};
+use solana_sdk::transaction::Transaction;
+
+use charity::{entrypoint::process_instruction, id, selector::ProgramSelector};
+use charity::{
+    storage::{DonationPDA, Bank},
+    DONATION_PDA_SEED,
+};
+
+struct RuntimeEnviroment {
+    ctx: ProgramTestContext,
+    admin: Keypair,
+    admin: Keypair
+}
+
+impl RuntimeEnviroment {
+    fn init() -> Self {
+        let program = ProgramTest::new("charity", id(), processor!(process_instruction));
+        let mut ctx = program.start_with_context().await;
+
+        let owner = Keypair::new();
+        let user = Keypair::new();
+
+        ctx.banks_client.process_transaction(
+            Transaction::new_signed_with_payer(
+                &[
+                    system_instruction::transfer(&ctx.payer, &owner, 1**9),
+                    system_instruction::transfer(&ctx.payer, &user, 1**9)
+                ], 
+                Some(&ctx.payer.pubkey()),
+                &ctx.payer,
+                ctx.last_block_hash
+            )
+        ).await.unwrap();
+
+        ctx.banks_client.process_transaction(
+            Transaction::new_signed_with_payer(
+                &[
+                    ProgramSelector::transfer_ownership(&owner, owner.clone())
+                ],
+                Some(&ctx.payer.pubkey()),
+                &ctx.payer,
+                ctx.last_block_hash
+            )
+        ).await.unwap();
+
+        let acc = ctx.banks_client.get_account(Bank::get_bank_pubkey()).await.unwrap().unwrap();
+        let bank = Bank::try_from_slice(acc.data.as_slice()).unwrap();
+
+        // init user's donation pda
+        let allocSpace = DonationPDA { total_donated: 0 }.try_to_vec().unwrap().len();
+        let rent = ctx.banks_client.get_rent().await.unwrap();
+        let lamports = rent.minimum_balance(allocSpace);
+        let instruction = system_instruction::create_account_with_seed(
+            &user.pubkey(),
+            &DonationPDA::get_donation_pda_pubkey(&user.pubkey()),
+            &user.pubkey(),
+            DONATION_PDA_SEED,
+            lamports,
+            allocSpace as u64,
+            &id(),
+        );
+        let tx = Transaction::new_signed_with_payer(
+            &[instruction],
+            Some(&user.pubkey()),
+            &[&user],
+            ctx.last_blockhash,
+        );
+        ctx.banks_client.process_transaction(tx).await.unwrap();
+
+        RuntimeEnviroment { ctx, admin, user }
+    }
+}
